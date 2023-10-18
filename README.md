c527dc258a0efb111e9f70afaff31fd7725827ef
Lucka <luckabarbeau@gmail.com>
2023-10-18 13:46:41 -0400

    improved read me adjust case

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# particle_pair_sedimentation_dynamic
+#  Pair of particles sedimentation ROM
+This repository includes the force models (induced drag, induced lift, induced torque) for a pair of particles and a reduced order model (ROM) for the sedimentation of this pair of particles.
+The ROM comes with an example of a case. To launch the example, simply execute the code contained in the main.py file.
--- main.py
+++ main.py
@@ -17,15 +17,15 @@ tf.random.set_seed(
 
 
 # Initialize the model
-rom_model=ROM_2_particles(final_time=0.4,time_step=0.00025)
+rom_model=ROM_2_particles(final_time=0.6,time_step=0.00025)
 
 #Setup the case 
-# units for length (millimeters ), units for time (second), units for mass (Kilograms)
+# units for length (centimeter), units for time (second), units for mass (Kilograms)
 
 
-rom_model.gravity=np.array([0,0,-10000])
-rom_model.fluid.rho=0.000001
-rom_model.fluid.mu=0.00070035 
+rom_model.gravity=np.array([0,0,-1000])
+rom_model.fluid.rho=0.0001
+rom_model.fluid.mu=0.000175 
 rom_model.particle1.position=np.array([0.0,0.0,0])
 rom_model.particle2.position=np.array([0.0,0.5,2.0])
 rom_model.particle1.rho=0.001
@@ -46,7 +46,7 @@ ax1 = fig1.add_subplot()
 ax1.plot(output.time_table,output.vectorize_components(output.p1_velocity,1),label=r"$P_0$ ROM", color='k')
 ax1.plot(output.time_table,output.vectorize_components(output.p2_velocity,1),label=r"$P_1$ ROM", color='b')
 ax1.set_xlabel(r"t (s)")
-ax1.set_ylabel(r"$v_y$ $\frac{mm}{s}$")
+ax1.set_ylabel(r"$v_y$ $\frac{cm}{s}$")
 ax1.legend()
 fig1.tight_layout()
 
@@ -56,7 +56,7 @@ ax2 = fig2.add_subplot()
 ax2.plot(output.time_table,output.vectorize_components(output.p1_velocity,2),label=r"$P_0$ ROM", color='k')
 ax2.plot(output.time_table,output.vectorize_components(output.p2_velocity,2),label=r"$P_1$ ROM", color='b')
 ax2.set_xlabel(r"t (s)")
-ax2.set_ylabel(r"$v_z$ $\frac{mm}{s}$")
+ax2.set_ylabel(r"$v_z$ $\frac{cm}{s}$")
 
 ax2.legend()
 fig2.tight_layout()    
